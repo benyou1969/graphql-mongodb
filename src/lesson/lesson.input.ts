0d7cccea51a050dd209a37d551f1f41822365e82
benyou1969 <benyou.11@hotmail.com>
2020-06-02 16:26:22 +0100

    Validation Create Lesson Input

---
+++ src/lesson/lesson.input.ts
@@ -0,0 +1,16 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { MinLength, IsDateString } from 'class-validator';
+@InputType()
+export class CreateLessonInput {
+  @MinLength(1)
+  @Field()
+  name: string;
+
+  @IsDateString()
+  @Field()
+  startDate: string;
+
+  @IsDateString()
+  @Field()
+  endDate: string;
+}
--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,6 +1,7 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
@@ -12,10 +13,8 @@ export class LessonResolver {
 
   @Mutation(reutrn => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ) {
-    return this.lessonService.createLesson(name, startDate, endDate);
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
